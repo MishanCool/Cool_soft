fd70e1f330ec318c57c343de7894b01add351273
Mishan Madhupa <mishancool2016@gmail.com>
2019-11-27 23:25:46 +0530

    Style login page forgoten password link

--- Cool_soft/resources/views/auth/login.blade.php
+++ Cool_soft/resources/views/auth/login.blade.php
@@ -203,8 +203,8 @@
                                 @enderror
                         </div>
 
-                        <div class="d-md-flex" id="forgoten_password">
-                                <div class="form-group row">
+                        <div class="d-md-flex">
+                                <div class="form-group row" id="remember_password">
                                         <div class="col-md-6 offset-md-4">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -216,17 +216,20 @@
                                         </div>
                                     </div>
 
-                                    <div class="form-group row mb-0">
+                                    <div class="form-group row mb-0>
                                         <div class="col-md-8 offset-md-4">
                                             <button type="submit" class="btn btn-primary">
                                                 {{ __('Login') }}
                                             </button>
 
-                                            @if (Route::has('password.request'))
+                                            <div class="forgoten" id="forgoten_password">
+                                                @if (Route::has('password.request'))
                                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                                     {{ __('Forgot Your Password?') }}
                                                 </a>
                                             @endif
+                                            </div>
+
                                         </div>
                                     </div>
                         </div>
