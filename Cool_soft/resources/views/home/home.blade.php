52671eff11adf72fb2f82ad69de25813acbaa0b4
Mishan Madhupa <mishancool2016@gmail.com>
2019-11-27 22:47:28 +0530

    Connect login and registration frontend to  backend

--- Cool_soft/resources/views/home/home.blade.php
+++ Cool_soft/resources/views/home/home.blade.php
@@ -77,11 +77,95 @@
 	        	<li class="nav-item"><a href="{{ URL::to('/blog') }}" class="nav-link">Blog</a></li>
                 <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
                 <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign Up</a></li>
+                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Sign In</a></li>
 	        </ul>
 	      </div>
 	    </div>
 	  </nav>
     <!-- END nav -->
+
+
+    <section class="ftco-section ftco-team img" style="background-image:url(images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+    	<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row justify-content-center pb-5">
+          <div class="col-md-6 heading-section heading-section-white text-center ftco-animate">
+          	<span class="subheading">Team Squad</span>
+            <h2 class="mb-4">Our Team <span>Squad</span></h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+          </div>
+        </div>
+    		{{-- <div class="row">
+    			<div class="col-md-12 ftco-animate">
+    				<div class="carousel-team owl-carousel">
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/about.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Wide Receiver</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-2.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Tight End</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-3.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Defensive</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-4.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Quarterback</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-5.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Runing Back</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-6.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Center</span>
+		    				</div>
+	    				</div>
+    				</div>
+    				<div class="item">
+    					<div class="team-wrap text-center">
+		    				<div class="img" style="background-image: url(images/staff-7.jpg);"></div>
+		    				<div class="text">
+		    					<h3 class="mb-0">David Scott</h3>
+		    					<span class="position">Offensive Guard</span>
+		    				</div>
+	    				</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div> --}}
+    </section>
+
 
 
     <footer class="ftco-footer ftco-footer-2 ftco-section">
