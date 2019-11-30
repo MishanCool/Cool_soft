d76ff7b1b0e24a49f022336effaba2cd943dc3a4
Mishan Madhupa <mishancool2016@gmail.com>
2019-11-30 22:02:49 +0530

    Style profile card

--- Cool_soft/resources/views/users/Normal_users/normal_users.blade.php
+++ Cool_soft/resources/views/users/Normal_users/normal_users.blade.php
@@ -209,73 +209,111 @@
                             {{-- </div> --}}
 
                             {{-- <div class="col-md-8 ml-5"> --}}
+<section>
+        <div class="container-fluid">
+                <div class="row">
 
-                                        <section class="ftco-section ftco-no-pt ftco-no-pb contact-section">
-                                                <div class="container">
-                                                    <div class="row d-flex align-items-stretch no-gutters">
-                                            <div class="col-md-6 p-4 p-md-5 order-md-last bg-light">
+                    <div class="col-md-4">
+                            <div class="card" style="width: 200px; height:200px">
+                                    <img src="/w3images/team2.jpg" alt="{{ Auth::user()->name }}" style="width:100%">
+                                    <h1><a class="lable">                                      {{-- href="#" role="button" aria-haspopup="true" aria-expanded="false" v-pre --}}
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
+                                    </h1>
+                                        <p class="title">{{ Auth::user()->email }}</p>
 
-                                                <form action="{{URL::to('/order')}}" method="POST">
-                                                    @csrf
+                                        {{-- <div style="margin: 24px 0;">
+                                          <a href="#"><i class="fa fa-dribbble"></i></a>
+                                          <a href="#"><i class="fa fa-twitter"></i></a>
+                                          <a href="#"><i class="fa fa-linkedin"></i></a>
+                                          <a href="#"><i class="fa fa-facebook"></i></a>
+                                        </div> --}}
+                                        <p><button><a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">Sign Out</a>
 
-                                                  <div class="form-group">
-                                                    <input type="text" class="form-control" name="name" placeholder="Your Name">
-                                                  </div>
-                                                  <div class="form-group">
-                                                    <input type="text" class="form-control" name="email" placeholder="Your Email">
-                                                  </div>
-                                                  <div class="form-group">
-                                                    <input type="text" class="form-control" name="phone_num" placeholder="phone_num">
-                                                  </div>
-                                                  <div class="form-group">
-                                                        <input type="text" class="form-control" name="city" placeholder="city">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input type="text" class="form-control" name="country" placeholder="country">
-                                                    </div>
-                                                  <div class="form-group">
-                                                        <input type="text"  class="form-control" name="order" placeholder="Order">
-                                                    {{-- <textarea name="" id="" cols="30" rows="7" class="form-control" name="order" placeholder="order"></textarea> --}}
-                                                  </div>
-                                                  <div class="form-group">
-                                                    <input type="submit" value="Place Order" class="btn btn-primary py-3 px-5">
-                                                  </div>
-                                                </form>
-
-                                                        </div>
-                                                        {{-- <div class="col-md-6 d-flex align-items-stretch">
-                                                            <div id="map"></div>
-                                                        </div> --}}
-
-                                                        <div class="card">
-                                                                <img src="/w3images/team2.jpg" alt="{{ Auth::user()->name }}" style="width:100%">
-                                                                <h1><a class="lable" href="#" role="button" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                                                    </a>
-                                                                </h1>
-                                                                    <p class="title">{{ Auth::user()->email }}</p>
-
-                                                                    <div style="margin: 24px 0;">
-                                                                      <a href="#"><i class="fa fa-dribbble"></i></a>
-                                                                      <a href="#"><i class="fa fa-twitter"></i></a>
-                                                                      <a href="#"><i class="fa fa-linkedin"></i></a>
-                                                                      <a href="#"><i class="fa fa-facebook"></i></a>
-                                                                    </div>
-                                                                    <p><button><a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
-                                                                        document.getElementById('logout-form').submit();">Sign Out</a>
-
-                                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                                            @csrf
-                                                                        </form>
-                                                                    </button></p>
-                                                    </div>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </button></p>
+                        </div>
+                    </div>
 
 
+                    <div class="col-md-8">
+                            <section class="ftco-section ftco-no-pt ftco-no-pb contact-section">
+                                    <div class="container">
+                                        <div class="row d-flex align-items-stretch no-gutters">
+                                <div class="col-md-8 p-4 p-md-8 order-md-last bg-light">
+
+                                    <form action="{{URL::to('/order')}}" method="POST">
+                                        @csrf
+
+                                      <div class="form-group">
+                                        <input type="text" class="form-control" name="name" placeholder="Your Name">
+                                      </div>
+                                      <div class="form-group">
+                                        <input type="text" class="form-control" name="email" placeholder="Your Email">
+                                      </div>
+                                      <div class="form-group">
+                                        <input type="text" class="form-control" name="phone_num" placeholder="phone_num">
+                                      </div>
+                                      <div class="form-group">
+                                            <input type="text" class="form-control" name="city" placeholder="city">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="country" placeholder="country">
+                                        </div>
+                                      <div class="form-group">
+                                            <input type="text"  class="form-control" name="order" placeholder="Order">
+                                        {{-- <textarea name="" id="" cols="30" rows="7" class="form-control" name="order" placeholder="order"></textarea> --}}
+                                      </div>
+                                      <div class="form-group">
+                                        <input type="submit" value="Place Order" class="btn btn-primary py-3 px-5">
+                                      </div>
+                                    </form>
+
+                                            </div>
+                                            {{-- <div class="col-md-6 d-flex align-items-stretch">
+                                                <div id="map"></div>
+                                            </div> --}}
+
+                                        </div>
+                                    </div>
+                                </section>
+                    </div>
+
+                </div>
+            </div>
+</section>
+
+    {{-- **************************************************************************************************************************** --
+    <section class="ftco-section ftco-no-pt ftc-no-pb">
+			<div class="container">
+				<div class="row d-flex">
+					<div class="col-md-8 order-md-last wrap-about wrap-about d-flex align-items-stretch">
+
+                    </div>
 
 
-                                                    </div>
-                                                </div>
-                                            </section>
+					<div class="col-md-4 wrap-about py-5 pr-md-4 ftco-animate">
+
+                    </div>
+				</div>
+			</div>
+		</section>
+    {{-- **************************************************************************************************************************** --}}
+
+
+
+
+
+
+
+
+
+
+
+
 
                             {{-- </div> --}}
                     {{-- </div> --}}
@@ -283,6 +321,65 @@
 
 
                 {{-- </div> --}}
+
+
+                <footer class="ftco-footer ftco-footer-2 ftco-section">
+                    <div class="container">
+                      <div class="row mb-5">
+                        <div class="col-md">
+                          <div class="ftco-footer-widget mb-4">
+                            <h2 class="ftco-heading-2">DARK COOL</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                              <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                              <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                              <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div class="col-md">
+                          <div class="ftco-footer-widget mb-4">
+                              <h2 class="ftco-heading-2">Have a Questions?</h2>
+                              <div class="block-23 mb-3">
+                                <ul>
+                                  <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                                </ul>
+                              </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12 text-center">
+
+                          <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://darkcool.com" target="_blank">DarkCool</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                      </div>
+                    </div>
+                  </footer> <!-- END footer -->
+
+    <!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/scrollax.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+<script src="js/google-map.js"></script>
+<script src="js/main.js"></script>
 
 
 </body>
